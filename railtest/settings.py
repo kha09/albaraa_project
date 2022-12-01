e196e1f29b6303e76cd1139b93fb6e3a67949b8c
unknown <KHA@DESKTOP-413PLKT.(none)>
2022-12-01 19:18:32 +0300

    added postgres database

--- railtest/settings.py
+++ railtest/settings.py
@@ -79,12 +79,26 @@ WSGI_APPLICATION = 'railtest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'RUjUpH4cCaW23OZubs4J',
+        'HOST': 'containers-us-west-95.railway.app',
+        'PORT': '5852',
     }
 }
+
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
